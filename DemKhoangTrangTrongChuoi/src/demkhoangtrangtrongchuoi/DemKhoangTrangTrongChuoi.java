1b6406c0c9d7f2583c0244cec2e5e9ecd45a71f1
Vegetaz <vegetaz@outlook.com>
2018-09-21 15:50:10 +0700

    Đếm khoảng trắng trong chuỗi

---
+++ DemKhoangTrangTrongChuoi/src/demkhoangtrangtrongchuoi/DemKhoangTrangTrongChuoi.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demkhoangtrangtrongchuoi;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Vegetaz
+ */
+public class DemKhoangTrangTrongChuoi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String s;
+        char c;
+        int n = 0;
+        Scanner sn = new Scanner(System.in);
+        System.out.print("Nhập vào một chuỗi ký tự: ");
+        s = sn.nextLine();
+        for (int i = 0; i < s.length(); i++) {
+            c = s.charAt(i);
+            if(c == " ") {
+                n++;
+            }
+        }
+        System.out.println("Số khoảng trắng trong chuỗi: " + n);
+    }
+    
+}
