651a957cc2969fb4e0389d8c6277904799616b17
Vegetaz <vegetaz@outlook.com>
2018-09-23 21:42:33 +0700

    Đếm khoảng trắng trong chuỗi ký tự nhập vào

--- DemKhoangTrangTrongChuoi/src/demkhoangtrangtrongchuoi/DemKhoangTrangTrongChuoi.java
+++ DemKhoangTrangTrongChuoi/src/demkhoangtrangtrongchuoi/DemKhoangTrangTrongChuoi.java
@@ -25,11 +25,11 @@ public class DemKhoangTrangTrongChuoi {
         s = sn.nextLine();
         for (int i = 0; i < s.length(); i++) {
             c = s.charAt(i);
-            if(c == ' ') {
+            if (c == ' ') {
                 n++;
             }
         }
-        System.out.println("Số khoảng trắng trong chuỗi " + "\"" + s + "\"" + "là: " + n);
+        System.out.println("Số khoảng trắng trong chuỗi " + "\"" + s + "\"" + " là: " + n);
     }
-    
+
 }
