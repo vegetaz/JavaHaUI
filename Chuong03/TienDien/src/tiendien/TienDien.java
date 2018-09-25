4f689fa0f7e87ef764ab40084b8984857201e6fd
Vegetaz <vegetaz@outlook.com>
2018-09-25 22:41:02 +0700

    Tiền điện Demo

--- Chuong03/TienDien/src/tiendien/TienDien.java
+++ Chuong03/TienDien/src/tiendien/TienDien.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tiendien;
 
 import java.util.Scanner;
 
-/**
- *
- * @author nghien
- */
 public class TienDien {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         int sodien;
         final int BASE1 = 50;
@@ -26,23 +14,18 @@ public class TienDien {
         final double RATE3 = 2000;
         final double RATE4 = 3000;
         Scanner sn = new Scanner(System.in);
-        System.out.print("Moi ban nhap so dien tieu thu:");
+        System.out.print("Số điện tiêu thụ: ");
         sodien = sn.nextInt();
-        double tongtien = 0;
-        if(sodien<=BASE1){
-            tongtien = sodien*RATE1;
+        double tongtien = 0d;
+        if (sodien <= BASE1) {
+            tongtien = sodien * RATE1;
+        } else if (sodien <= BASE2) {
+            tongtien = BASE1 * RATE1 + (sodien - BASE1) * RATE2;
+        } else if (sodien <= BASE3) {
+            tongtien = BASE1 * RATE1 + BASE1 * RATE2 + (sodien - 2 * BASE1) * RATE3;
+        } else {
+            tongtien = BASE1 * RATE1 + BASE1 * RATE2 + BASE1 * RATE3 + (sodien - 3 * BASE1) * RATE4;
         }
-        else if(sodien<=BASE2){
-            tongtien = BASE1*RATE1+(sodien-BASE1)*RATE2;
-        }
-        else if(sodien<=BASE3){
-            tongtien = BASE1*RATE1+BASE1*RATE2+(sodien-2*BASE1)*RATE3;
-        }
-        else{
-            tongtien = BASE1*RATE1+BASE1*RATE2+BASE1*RATE3+(sodien-3*BASE1)*RATE4;
-        }
-        System.out.println("Tổng tiền điện phải trả là: "+tongtien);
-        
+        System.out.println("Tổng tiền điện phải trả là: " + tongtien);
     }
-    
 }
