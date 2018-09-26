f2a967399ef393c9b18c2b79ceaaf9bc481b8684
Vegetaz <vegetaz@outlook.com>
2018-09-26 10:19:47 +0700

    Các kiểu dữ liệu trong Java

---
+++ Chuong01/CacKieuDuLieu/src/cackieudulieu/CacKieuDuLieu.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cackieudulieu;
+
+/**
+ *
+ * @author Vegetaz
+ */
+public class CacKieuDuLieu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.printf("Type: %-10s Size: %-2s Min: %-20s Max: %-20s\n", 
+                Integer.TYPE, Integer.SIZE, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        System.out.printf("Type: %-10s Size: %-2s Min: %-20s Max: %-20s\n", 
+                Short.TYPE, Short.SIZE, Short.MIN_VALUE, Short.MAX_VALUE);
+        System.out.printf("Type: %-10s Size: %-2s Min: %-20s Max: %-20s\n", 
+                Long.TYPE, Long.SIZE, Long.MIN_VALUE, Long.MAX_VALUE);
+        System.out.printf("Type: %-10s Size: %-2s Min: %-20s Max: %-20s\n", 
+                Byte.TYPE, Byte.SIZE, Byte.MIN_VALUE, Byte.MAX_VALUE);
+        System.out.printf("Type: %-10s Size: %-2s Min: %-20s Max: %-20s\n", 
+                Float.TYPE, Float.SIZE, Float.MIN_VALUE, Float.MAX_VALUE);
+        System.out.printf("Type: %-10s Size: %-2s Min: %-20s Max: %-20s\n", 
+                Double.TYPE, Double.SIZE, Double.MIN_VALUE, Double.MAX_VALUE);
+        System.out.printf("Type: %-10s Size: %-2s Min: %-20s Max: %-20s\n", 
+                Character.TYPE, Character.SIZE, Character.MIN_VALUE, Character.MAX_VALUE);
+        boolean isNumber; //True hoặc False
+    }
+    
+}
