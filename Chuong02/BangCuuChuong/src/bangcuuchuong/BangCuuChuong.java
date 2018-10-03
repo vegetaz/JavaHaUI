0802c1baa1a93da6942d685d2a383f363a4190a3
Vegetaz <vegetaz@outlook.com>
2018-10-03 22:47:57 +0700

    Bảng cửu chương

--- Chuong02/BangCuuChuong/src/bangcuuchuong/BangCuuChuong.java
+++ Chuong02/BangCuuChuong/src/bangcuuchuong/BangCuuChuong.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package bangcuuchuong;
 
 /**
  *
@@ -15,11 +9,12 @@ public class BangCuuChuong {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++){
-            for (int j = 1; j <= 10; j++){
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
                 System.out.print(i + " x " + j + " = " + i * j + "\t");
-            }System.out.println("");
+            }
+            System.out.println("");
         }
     }
-    
+
 }
