e483976874fc5cd326bdcf66be6c6aff2a022a36
Vegetaz <vegetaz@outlook.com>
2018-10-31 23:42:19 +0700

    Sắp xếp sinh viên updated

--- Chuong06/SinhVienDemo/src/sinhviendemo/SortSinhVien.java
+++ Chuong06/SinhVienDemo/src/sinhviendemo/SortSinhVien.java
@@ -15,18 +15,20 @@ import java.sql.SQLException;
  * @author Vegetaz
  */
 public class SortSinhVien {
+
     SinhVienConnection svcon = new SinhVienConnection();
     PreparedStatement pst;
     Connection conn = svcon.getConnection();
+
     public void sortSV() {
         String sql = "SELECT * FROM tblsinhvien ORDER BY name"; //tăng dần
 //        String sql = "SELECT * FROM tblsinhvien ORDER BY name DESC"; //giảm dần
         try {
             pst = conn.prepareStatement(sql);
             ResultSet rs = pst.executeQuery();
-            System.out.println("Danh sách sinh viên sau khi sắp xếp");
+            System.out.println("Danh sách sinh viên sau khi sắp xếp theo tên");
             System.out.printf("%-12s%-18s%-6s%-25s%-18s\n", "ID", "Tên", "Tuổi", "Địa chỉ", "Điểm trung bình");
-            while(rs.next()) {
+            while (rs.next()) {
                 int id = rs.getInt("id");
                 String name = rs.getString("name");
                 int age = rs.getInt("age");
