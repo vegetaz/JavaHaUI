d74a1595dc6da429cfbc9b0a6b640261dedc6317
Vegetaz <vegetaz@outlook.com>
2018-11-01 23:04:05 +0700

    Cập nhật thêm chức năng

--- Chuong06/SinhVienDemo/src/sinhviendemo/SinhVienDemo.java
+++ Chuong06/SinhVienDemo/src/sinhviendemo/SinhVienDemo.java
@@ -18,6 +18,7 @@ public class SinhVienDemo {
      * @param args the command line arguments
      */
     static Scanner sc;
+
     public static void main(String[] args) {
         ViewSinhVien viewsv = new ViewSinhVien();
         ArrayList<SinhVien> as = viewsv.viewSV();
@@ -25,16 +26,18 @@ public class SinhVienDemo {
         DeleteSinhVien dels = new DeleteSinhVien();
         SortSinhVien sors = new SortSinhVien();
         SearchSinhVien sears = new SearchSinhVien();
+        SearchSinhVienT searst = new SearchSinhVienT();
         int choice;
         sc = new Scanner(System.in);
         do {
             System.out.println("===== Danh Sách Chức Năng =====");
             System.out.println("1. Thêm sinh viên mới");
             System.out.println("2. Hiển thị danh sách sinh viên");
-            System.out.println("3. Tìm kiếm sinh viên");
-            System.out.println("4. Sắp xếp sinh viên theo tên");
-            System.out.println("5. Xoá sinh viên");
-            System.out.println("6. Thoát chương trình");
+            System.out.println("3. Tìm kiếm sinh viên theo ID");
+            System.out.println("4. Tìm kiếm sinh viên theo tên");
+            System.out.println("5. Sắp xếp sinh viên theo tên");
+            System.out.println("6. Xoá sinh viên");
+            System.out.println("7. Thoát chương trình");
             System.out.print("Chọn chức năng: ");
             choice = sc.nextInt();
             switch (choice) {
@@ -48,16 +51,19 @@ public class SinhVienDemo {
                     sears.searchSV();
                     break;
                 case 4:
-                    sors.sortSV();
+                    searst.searchT();
                     break;
                 case 5:
-                    dels.deleteSV();
+                    sors.sortSV();
                     break;
                 case 6:
+                    dels.deleteSV();
+                    break;
+                case 7:
                     System.out.println("Thoát...");
                     break;
             }
-        } while (choice != 6);
+        } while (choice != 7);
     }
 
 }
