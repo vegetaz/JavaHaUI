8831d563435a7bce658c668a7b47dc8c341f4c76
Vegetaz <vegetaz@outlook.com>
2018-11-02 16:14:06 +0700

    Cập nhật sinh viên

--- Chuong06/SinhVienDemo/src/sinhviendemo/ViewSinhVien.java
+++ Chuong06/SinhVienDemo/src/sinhviendemo/ViewSinhVien.java
@@ -6,9 +6,9 @@
 package sinhviendemo;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 /**
@@ -19,29 +19,45 @@ public class ViewSinhVien {
 
     SinhVienConnection svcon = new SinhVienConnection();
     Connection conn = svcon.getConnection();
-    //Statement st;
     final String sql = "SELECT * FROM tblsinhvien";
-
-    public ArrayList<SinhVien> viewSV() {
+    PreparedStatement pst;
+    ResultSet rs;
+    
+    public ArrayList<SinhVien> getAllSinhVien() {
         ArrayList<SinhVien> asv = new ArrayList();
         try {
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery(sql);
-            System.out.println("Danh sách sinh viên");
-            System.out.printf("%-12s%-18s%-6s%-25s%-18s\n", "ID", "Tên", "Tuổi", "Địa chỉ", "Điểm trung bình");
+            pst = conn.prepareStatement(sql);
+            rs = pst.executeQuery();
             while (rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                int age = rs.getInt("age");
-                String address = rs.getString("address");
-                float gpa = rs.getFloat("gpa");
-//                SinhVien sv = new SinhVien(id, name, age, address, gpa);
-//                asv.add(sv);
-                System.out.printf("%-12s%-18s%-6s%-25s%-18s\n", id, name, age, address, gpa);
+                SinhVien sv = new SinhVien();
+//                int id = rs.getInt("id");
+//                String name = rs.getString("name");
+//                int age = rs.getInt("age");
+//                String address = rs.getString("address");
+//                float gpa = rs.getFloat("gpa");
+                sv.setId(rs.getInt("id"));
+                sv.setName(rs.getString("name"));
+                sv.setAge(rs.getInt("age"));
+                sv.setAddress(rs.getString("address"));
+                sv.setGpa(rs.getFloat("gpa"));
+                asv.add(sv);
+//                System.out.printf("%-12s%-18s%-6s%-25s%-18s\n", id, name, age, address, gpa);
             }
         } catch (SQLException ex) {
             System.out.println("Lỗi: " + ex.getMessage());
         }
         return asv;
+    }
+    
+    public void viewSinhVien() {
+        getAllSinhVien();
+        System.out.println("Danh sách sinh viên");
+        System.out.printf("%-12s%-18s%-6s%-25s%-18s\n", "ID", "Tên", "Tuổi", "Địa chỉ", "Điểm trung bình");
+//        for (SinhVien svi : getAllSinhVien()) {
+//            System.out.println(svi);
+//        }
+        getAllSinhVien().forEach((svi) -> {
+            System.out.println(svi);
+        });
     }
 }
